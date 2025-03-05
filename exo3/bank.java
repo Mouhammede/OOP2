b9a7680c407aecccfdbd8ae56177215da01be87a
mouhamed <righi.mohammedtahar@gmail.com>
2025-03-05 22:20:49 +0100

    update 2 adding exo3

---
+++ exo3/bank.java
@@ -0,0 +1,146 @@
+import java.util.ArrayList;
+public class bank 
+{
+    public String name;
+    public String locashon;
+    private ArrayList<account> accounts=new ArrayList<>();
+    public class account
+    {
+        private int ID;
+        private int password;
+        private double balence;
+        private ArrayList<history> his;
+        private double loan;
+        public double tax;
+        public account(int i,int pass,double bal)
+        {
+            ID=i;
+            password=pass;
+            balence=bal;
+            his=new ArrayList<>();
+            loan=0;
+            tax=20;
+        }
+        public boolean checkownership(int i,int pass)
+        {
+            if(ID==i&&password==pass)
+            {
+                return true;
+            }
+            return false;
+        }
+        public void deposit(double n)
+        {
+            date t=new date();
+            his.add(new history(t,"deposit",ID,n,0));
+            balence=balence+n-tax;
+        }
+        public void withdraw(double n)
+        {
+            date t=new date();
+            if(n<=balence)
+            {
+                balence=balence-n-tax;
+                his.add(new history(t,"Withdraw",ID,n,0));
+            }
+            else
+            {
+                System.out.println("you do not have "+n+" DA");
+                System.out.println("you have "+balence+" DA");
+            }
+        }
+        public void takeloan(double n)
+        {
+            if(loan==0)
+            {
+                loan=n;
+                date t=new date();
+                his.add(new history(t,"Take a loan",ID,n,0));
+            }
+        }
+        public void paytheloan(double n)
+        {
+            if(loan!=0)
+            {
+                date t=new date();
+                loan=loan-n;
+                if(loan<0)
+                {
+                    loan=loan*(-1);
+                    balence=balence+loan;
+                    his.add(new history(t,"pay the loan",ID,n-loan,0));
+                    his.add(new history(t,"Deposit",ID,loan,0));
+                    loan=0;
+                }
+                else
+                {
+                    his.add(new history(t,"pay the loan",ID,n-loan,0));
+                }
+            }
+        }
+    }
+    private int getindex(int id,int pass)
+    {
+        int i;
+        for(i=0;i<accounts.size();i++)
+        {
+            if(accounts.get(i).checkownership(id,pass))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public void dipos(double n,int id,int pass)
+    {
+        int y;
+        y=getindex(id,pass);
+        if(y!=-1)
+        {
+            accounts.get(y).deposit(n);
+        }
+        else
+        {
+            System.out.println("ID or password incoracte");
+        }
+    }
+    public void withd(double n,int id,int pass)
+    {
+        int y;
+        y=getindex(id,pass);
+        if(y!=-1)
+        {
+            accounts.get(y).withdraw(n);
+        }
+        else
+        {
+            System.out.println("ID or password incoracte");
+        }
+    }
+    public void tloan(double n,int id,int pass)
+    {
+        int y;
+        y=getindex(id,pass);
+        if(y!=-1)
+        {
+            accounts.get(y).takeloan(n);
+        }
+        else
+        {
+            System.out.println("ID or password incoracte");
+        }
+    }
+    public void ploan(double n,int id,int pass)
+    {
+        int y;
+        y=getindex(id,pass);
+        if(y!=-1)
+        {
+            accounts.get(y).paytheloan(n);
+        }
+        else
+        {
+            System.out.println("ID or password incoracte");
+        }
+    }
+}
