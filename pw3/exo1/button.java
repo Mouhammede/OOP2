002972c0b4886c8dc2b76fafea563264c2c59f16
mouhamed <righi.mohammedtahar@gmail.com>
2025-03-22 00:09:40 +0100

    finished grafic (GUI)

--- pw3/exo1/button.java
+++
@@ -1,9 +0,0 @@
-import java.awt.Dimension;
-import javax.swing.JButton;
-public class button extends JButton{
-    public button(String s)
-    {
-        this.setText(s);
-        this.setPreferredSize(new Dimension(50,25));
-    }
-}
